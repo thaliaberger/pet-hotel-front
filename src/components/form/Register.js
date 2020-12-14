17aeca068f1556b7d0c6b20de891f8b1023bd554
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-14 14:46:46 -0300

    fix build

--- src/components/form/Register.js
+++ src/components/form/Register.js
@@ -5,56 +5,56 @@ import Btn from "./Btn";
 import { useHistory } from "react-router-dom";
 
 function Register() {
-  const [state, setState] = useState({ email: "", password: "" });
-  const [errors, setErrors] = useState({
-    email: null,
-    password: null,
-  });
-  const history = useHistory();
+	const [state, setState] = useState({ email: "", password: "" });
+	const [errors, setErrors] = useState({
+		email: null,
+		password: null,
+	});
+	const history = useHistory();
 
-  function handleChange(event) {
-    setState({ ...state, [event.target.name]: event.target.value });
-  }
+	function handleChange(event) {
+		setState({ ...state, [event.target.name]: event.target.value });
+	}
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://ec2-52-14-163-166.us-east-2.compute.amazonaws.com/api/user/signup",
-        state
-      );
-      console.log(response.data);
-      history.push("/");
-    } catch (error) {
-      console.log(error.response);
-      setErrors({ ...error.response.data.errors });
-    }
-  }
+	async function handleSubmit(event) {
+		event.preventDefault();
+		try {
+			const response = await axios.post(
+				"http://ec2-52-14-163-166.us-east-2.compute.amazonaws.com/api/user/signup",
+				state
+			);
+			console.log(response.data);
+			history.push("/");
+		} catch (error) {
+			console.log(error.response);
+			setErrors({ ...error.response.data.errors });
+		}
+	}
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <SimpleInput
-        name="email"
-        label="e-mail"
-        type="text"
-        value={state.email}
-        onChange={handleChange}
-        error={errors.email}
-      />
-      <SimpleInput
-        name="password"
-        label="Password"
-        type="password"
-        value={state.password}
-        onChange={handleChange}
-        error={errors.password}
-      />
-      <div className="botoes-cadastro">
-        <Btn type="submit" color="laranja" label="Cadastrar" type="submit" />
-        <Btn targetUrl="/" color="azul" label="Voltar" />
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<SimpleInput
+				name="email"
+				label="e-mail"
+				type="text"
+				value={state.email}
+				onChange={handleChange}
+				error={errors.email}
+			/>
+			<SimpleInput
+				name="password"
+				label="Password"
+				type="password"
+				value={state.password}
+				onChange={handleChange}
+				error={errors.password}
+			/>
+			<div className="botoes-cadastro">
+				<Btn type="submit" color="laranja" label="Cadastrar" />
+				<Btn targetUrl="/" color="azul" label="Voltar" />
+			</div>
+		</form>
+	);
 }
 
 export default Register;
