5f59799da51352208183f72a1dc8058b59aa421b
Rodrigo Matos <rodrigomatosrj@gmail.com>
2020-12-15 14:55:17 -0300

    booking in progress

--- src/components/cadastro/Register.js
+++ src/components/cadastro/Register.js
@@ -1,22 +1,16 @@
 import React from "react";
 import FormRegister from "../form/Register";
 import "./Cadastro.css";
-import { Link } from "react-router-dom";
 
 function Register() {
-  return (
-    <div className="container">
-      <div className="cadastro">
-        <h2>Cadastre-se</h2>
-
-        <FormRegister />
-
-        <Link className="google-register" to="/api/auth/google">
-          Cadastrar com Google
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="cadastro">
+				<h2>Cadastre-se</h2>
+				<FormRegister />
+			</div>
+		</div>
+	);
 }
 
 export default Register;
