c9adbf19655a836a983b2c0e0299b0e493d0fdb3
thaliaberger <thaliaberger@hotmail.com>
2020-12-17 17:15:29 -0300

    css homepage

--- src/components/homepage/SignIn.js
+++ src/components/homepage/SignIn.js
@@ -3,34 +3,36 @@ import Btn from "../form/Btn";
 import Login from "../form/Login";
 
 function SignIn() {
-	const [error, setError] = useState(false);
+  const [error, setError] = useState(false);
 
-	return (
-		<div className="home-info">
-			<Login setError={setError}/>
-			<div className="barra">
-				{error ? (
-					<div className="error">
-						<small>
-							<p>
-								Credenciais inválidas.
-								<br />
-								Tente novamente ou cadastre-se abaixo.
-							</p>
-						</small>
-					</div>
-				) : (
-					<></>
-				)}
-				<p>———————————</p>
-			</div>
+  return (
+    <div className="home-info">
+      <Login setError={setError} />
+      <div className="barra">
+        {error ? (
+          <div className="error">
+            <small>
+              <p>
+                Credenciais inválidas.
+                <br />
+                Tente novamente ou cadastre-se abaixo.
+              </p>
+            </small>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="barra-home">
+          <p>———————————</p>
+        </div>
+      </div>
 
-			<small>
-				<p>Ainda não tem conta?</p>
-			</small>
-			<Btn targetUrl="/register" color="azul" label="Cadastre-se" />
-		</div>
-	);
+      <small>
+        <p>Ainda não tem conta?</p>
+      </small>
+      <Btn targetUrl="/register" color="azul" label="Cadastre-se" />
+    </div>
+  );
 }
 
 export default SignIn;
