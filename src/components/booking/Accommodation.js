60417273ea23cd9606bf8424072374f63a9b9817
thaliaberger <thaliaberger@hotmail.com>
2020-12-17 18:18:09 -0300

    final version

--- src/components/booking/Accommodation.js
+++ src/components/booking/Accommodation.js
@@ -11,14 +11,16 @@ function Accommodation(props) {
       onClick={props.onClick}
       preco={props.value}
     >
-      <div className="room-title">
-        <h4>{props.name}</h4>
-        <img className="room-img" src={props.imageUrl} alt="#" />
-      </div>
-      <div className="room-info">
-        <p>Valor da diária: R$ {props.value.toFixed(2)}</p>
-        <p>Tipo da acomodação: {props.type} </p>
-        <p>Acomoda até {props.qtd} pets</p>
+      <div className="room-all-info">
+        <div className="room-title">
+          <h4>{props.name}</h4>
+          <img className="room-img" src={props.imageUrl} alt="#" />
+        </div>
+        <div className="room-info">
+          <p>Valor da diária: R$ {props.value.toFixed(2)}</p>
+          <p>Tipo da acomodação: {props.type} </p>
+          <p>Acomoda até {props.qtd} pets</p>
+        </div>
       </div>
     </div>
   );
